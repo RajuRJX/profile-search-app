5f5aafcacf1b11384b28803059e7ad9859219df9
RajuRJX <126534396+RajuRJX@users.noreply.github.com>
2023-10-27 00:17:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const argv = require("yargs")
+  .usage("Usage: $0 -p [port]")
+  .alias("p", "port")
+  .describe("port", "(optional) Port Number -default is 3000")
+  .strict().argv;
 
+const DEFAULT_PORT = 5800;
+const app = express();
+let port = DEFAULT_PORT;
+if (argv.p) {
+  port = argv.p;
+}
+
+app.use(morgan("dev"));
+app.use(
+  "/lib",
+  express.static(path.join(__dirname, "../../lib/msal-browser/lib"))
+);
+
+app.use(
+  "/lib",
+  express.static(path.join(__dirname, "../../lib/msal-browser/lib"))
+);
+
+// Serve static files from the "app" directory
+app.use(express.static("app"));
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(express.static("app"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port);
+console.log(`Listening on port ${port}.....`);
